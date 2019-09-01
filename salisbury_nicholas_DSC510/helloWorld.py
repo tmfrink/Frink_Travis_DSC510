5768fd2f796e09f098042cf9c8b655b54f496c82
Mike Eller <mike.eller65@gmail.com>
2019-08-31 22:09:57 -0500

    test

---
+++ salisbury_nicholas_DSC510/helloWorld.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# define our main function
+def main():
+    # Print hello world
+    print('Hello World!')
+
+# Call our main function
+if __name__ == '__main__':
+    main()
